dda4136d2ec09be4cb31e22ea97793498d8fd2ac
Johannes Boyne <johannes@boyne.de>
2014-02-24 22:26:25 +0100

    RF

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,58 +6,67 @@ colors = require('colors'),
 url = require('url');
 
 function error (type) {
-    switch (type) {
-        case 1:
-            console.error("Wrong Type of Arguments!");
-        break;
-        default:
-            console.error("Error");
-        break;
-    }
+  switch (type) {
+    case 1:
+      console.error("Wrong Type of Arguments!");
+    break;
+    default:
+      console.error("Error");
+    break;
+  }
 }
 
 if (argv.store && argv.at) {
-    var parsedStorage = url.parse(argv.at);
-    console.log('store to %s on %s for %s', parsedStorage.hostname, parsedStorage.port);
-    var req = http.request({
-        hostname: parsedStorage.hostname,
-        port: parsedStorage.port || 80,
-        path: parsedStorage.path,
-        method: 'PUT'
-    }, function(res) {
-        res.setEncoding('utf8');
-        var body = '';
-        res.on('data', function (chunk) {
-            body += chunk.toString();
-        });
-        res.on('end', function () {
-            if (body.replace(/\s/g, '') === argv.store.replace(/\s/g, ''))
-                console.info('√'.bold.green + ' stored'.green);
-            else
-                console.log('x storing error');
-        });
+  var parsedStorage = url.parse(argv.at);
+  console.log('store to %s on %s for %s', parsedStorage.hostname, parsedStorage.port);
+  var req = http.request({
+    hostname: parsedStorage.hostname,
+    port: parsedStorage.port || 80,
+    path: parsedStorage.path,
+    method: 'PUT'
+  }, function(res) {
+    res.setEncoding('utf8');
+    var body = '';
+    res.on('data', function (chunk) {
+      body += chunk.toString();
     });
-
-    req.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
+    res.on('end', function () {
+      if (body.replace(/\s/g, '') === argv.store.replace(/\s/g, ''))
+        console.info('√'.bold.green + ' stored'.green);
+      else
+        console.log('x storing error');
     });
+  });
 
-    req.setHeader('Content-Type', 'application/json');
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
 
-    // write data to request body
-    req.write(argv.store);
-    req.end();
+  req.setHeader('Content-Type', 'application/json');
+
+  // write data to request body
+  req.write(argv.store);
+  req.end();
 } else if (argv.follow && argv.at) {
-    var parsedStorage = url.parse(argv.at);
-    console.log('connect to %s on %s', parsedStorage.hostname, parsedStorage.port);
-    var client = net.connect({port: 8124, host: parsedStorage.hostname},
-                             function() { //'connect' listener
-                                 client.write('update:'+argv.follow);
-                             });
-    client.on('data', function(data) {
-        console.log('->'.bold.blue + ' received data:'.blue, data.toString().grey);
-    });
+  var parsedStorage = url.parse(argv.at);
+  console.log('connect to %s on %s', parsedStorage.hostname, parsedStorage.port);
+  var client = net.connect({port: 8124, host: parsedStorage.hostname},
+                           function() { //'connect' listener
+                             client.write(JSON.stringify({zen: { update: true, name: argv.follow }}));
+                             client.write('\n');
+                           });
+                           client.on('data', function(data) {
+                             console.log('->'.bold.blue + ' received data:'.blue, data.toString().grey);
+                           });
+} else if (argv.pipe && argv.at) {
+  var parsedStorage = url.parse(argv.at);
+  console.log('connect to %s on %s', parsedStorage.hostname, parsedStorage.port);
+  var client = net.connect({port: 8124, host: parsedStorage.hostname},
+                           function() { //'connect' listener
+                             client.write(JSON.stringify({zen: { pipeData: true, id: parsedStorage.path.replace(/\//, '') }}));
+                             process.stdin.pipe(client);
+                           });
 } else {
-    var fs = require('fs');
-    fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
+  var fs = require('fs');
+  fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
 }

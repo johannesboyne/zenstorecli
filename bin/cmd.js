f16e9d780f530a1c4b7ca7c8761995b98c411224
Johannes Boyne <johannes@boyne.de>
2014-02-27 17:02:52 +0100

    Eat my own dogfood ;)

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,7 +3,8 @@ var argv = require('yargs').argv,
 http = require('http'),
 net = require('net'),
 colors = require('colors'),
-url = require('url');
+url = require('url'),
+zenstorejs = require('zenstorejs');
 
 function error (type) {
   switch (type) {
@@ -18,54 +19,28 @@ function error (type) {
 
 if (argv.store && argv.at) {
   var parsedStorage = url.parse(argv.at);
-  console.log('store to %s on %s for %s', parsedStorage.hostname, parsedStorage.port);
-  var req = http.request({
-    hostname: parsedStorage.hostname,
-    port: parsedStorage.port || 80,
-    path: parsedStorage.path,
-    method: 'PUT'
-  }, function(res) {
-    res.setEncoding('utf8');
-    var body = '';
-    res.on('data', function (chunk) {
-      body += chunk.toString();
-    });
-    res.on('end', function () {
-      if (body.replace(/\s/g, '') === argv.store.replace(/\s/g, ''))
-        console.info('√'.bold.green + ' stored'.green);
-      else
-        console.log('x storing error');
-    });
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-  });
-
-  req.setHeader('Content-Type', 'application/json');
-
-  // write data to request body
-  req.write(argv.store);
-  req.end();
+  console.log('store to %s on %s for %s', parsedStorage.hostname, parsedStorage.port, argv.store);
+  zenstorejs.store(argv.at, argv.store, function (err, ret) {
+    if (!err)
+      console.info('√'.bold.green + ' stored'.green);
+    else
+      console.log('x storing error');
+  }); 
 } else if (argv.follow && argv.at) {
   var parsedStorage = url.parse(argv.at);
-  console.log('connect to %s on %s', parsedStorage.hostname, parsedStorage.port);
-  var client = net.connect({port: 8124, host: parsedStorage.hostname},
-                           function() { //'connect' listener
-                             client.write(JSON.stringify({zen: { update: true, name: argv.follow }}));
-                             client.write('\n');
-                           });
-                           client.on('data', function(data) {
-                             console.log('->'.bold.blue + ' received data:'.blue, data.toString().grey);
-                           });
+  console.log('follow @ %s on %s', parsedStorage.hostname, argv.port || 8124);
+  zenstorejs.follow(argv.follow, argv.at, function (stream) {
+    stream.on('data', function (data) {
+      console.log('->'.bold.blue + ' received data:'.blue, data.toString().grey);
+    });
+  });
+
 } else if (argv.pipe && argv.at) {
   var parsedStorage = url.parse(argv.at);
-  console.log('connect to %s on %s', parsedStorage.hostname, parsedStorage.port);
-  var client = net.connect({port: 8124, host: parsedStorage.hostname},
-                           function() { //'connect' listener
-                             client.write(JSON.stringify({zen: { pipeData: true, id: parsedStorage.path.replace(/\//, '') }}));
-                             process.stdin.pipe(client);
-                           });
+  console.log('pipe to %s', parsedStorage.hostname);
+  zenstorejs.createWritePipe(argv.at, function (zenPipe) {
+    process.stdin.pipe(zenPipe);
+  });
 } else {
   var fs = require('fs');
   fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
